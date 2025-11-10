77b6707509348a5c1128ac18c05aad4dfc0c753c
Albasan08 <sandin.alba@gmail.com>
2025-11-10 14:15:02 +0100

    ADD: Varios

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// VARIABLES
+
+const sectionAPintar = document.querySelector("#imagenes-mostradas");
+//console.log(sectionAPintar);
+
+let favorito = [];
+//console.log(favorito);
+
+// EVENTOS
+
+
+// FUNCIONES
+
+/* Agregar producto*/
+/* Eliminar producto */
+/* 
+
+
+// INVOCACIONES
+
+
+// Pseudocódigo
+/* 
+Pintar imágenes() - Pendiente crear el botón
+
+Clic en el botón - Evento ()
+
+Clic en el botón 
+Leer input
+Comprobar local storage
+    Si hay 0 ud
+        Añadir 1
+    Si hay 1 ud
+        Restar 1
+
+Pop up
+
+Pintar imágenes en pop up
+
+Se necesita
+Variable vacía
+Poder acceder al elemento del DOM donde está el icono
+Poder acceder al elemento del DOM del icono
+
+*/
